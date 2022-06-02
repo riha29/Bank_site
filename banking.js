8408defc18329b49241b53a89a76acb3fe43185b
Faozia Islam Riha <faozia.islam.29@gmail.com>
2022-06-03 00:13:32 +0600

    deposit and balance update

--- banking.js
+++ banking.js
@@ -1,16 +1,32 @@
 //handle deposite button event
 document.getElementById('deposite-button').addEventListener('click', function(){
     // amount deposited
-    const depositeInput = document.getElementById('deposite-input')
-    const newDepositAmount = depositeInput.value
+    const depositInput = document.getElementById('deposite-input')
+    const newDepositAmount = depositInput.value
     // console.log(depositeAmount)
 
     const depositTotal = document.getElementById('deposite-total')
     const previousDepositAmount = depositTotal.innerText
-    const newDepositTotal= previousDepositAmount+ newDepositAmount
-    const 
-    depositTotal.innerText = newDepositAmount
+    const newDepositTotal= parseFloat(previousDepositAmount)+ parseFloat(newDepositAmount)
+    depositTotal.innerText = newDepositTotal
+
+    //update withdraw
+    const withdrawInput = document.getElementById('withdraw-input')
+    const newWithdrawAmount = withdrawInput.value
+    console.log(newWithdrawAmount)
+
+    const withdrawTotal = document.getElementById('withdraw-total')
+    const firstWithdrawTotal = withdrawTotal.innerText
+    const newWithdrawTotal = parseFloat(firstWithdrawTotal)+ parseFloat(newWithdrawAmount)
+    withdrawTotal.innerText= newWithdrawTotal
+
+    //update balance
+    const balanceTotal = document.getElementById('balance-total')
+    const previousBalanceTotal = balanceTotal.innerText
+    const newBalanceTotal = parseFloat(previousBalanceTotal)+ parseFloat(newDepositAmount)
+    balanceTotal.innerText= newBalanceTotal
 
     //clear the dep input field
-    depositeInput.value=''
+    depositInput.value=''
+    withdrawInput.value=''
 })
